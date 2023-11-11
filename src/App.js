a505d3ff8283ace88d5a097f3634273349200084
Isaac Huffman <122467839+isaachuffman37@users.noreply.github.com>
2023-11-11 14:20:37 -0700

    comments in the app.js

--- src/App.js
+++ src/App.js
@@ -7,26 +7,33 @@ import DisplayToDoTasks from './DisplayToDoTasks';
 function App() {
   const[data, setData] = useState({items: []})
 
+  // add the task to the the data
   const addTask = (task) =>{
     let tasks = data["items"]
     tasks.push(task)
+    // sets the data so that the paeg rerenders with updated data
     setData({items: tasks})
   }
 
+  // this function deletes the task from data
   const deleteTask = (task) =>{
     let tasks = data["items"]
     const index = tasks.indexOf(task)
+    // deletes the task from the list at the index
     tasks.splice(index,1)
     setData({items:tasks})
   }
 
+  // changes the task variable to done
   const markAsDone = (task) =>{
     let tasks = data["items"];
     const index = tasks.indexOf(task);
+    // changes to done
     tasks[index].done = true;
     setData({items:tasks})
   }
 
+  //returns the list of the tasks that are not done
   const filterNotDone = (tasks) =>{
     let notDoneTasks = [] 
 
@@ -38,7 +45,7 @@ function App() {
     return notDoneTasks
   }
 
-
+  // returns a list of the done tasks
   const filterDone = (tasks) =>{
     let doneTasks = [] 
 
@@ -50,7 +57,8 @@ function App() {
     return doneTasks
   }
 
-
+  // returns the components that I created and the html and css associated with them
+  // the properties are elements that can be accessed in the component file
   return (
     <div className="App">
       <h1 className='header'>WELCOME TO THE TASK MANAGER!</h1>
@@ -63,4 +71,5 @@ function App() {
   );
 }
 
+// exports the app so that index.js can call it
 export default App;
